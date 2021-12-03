a8e2fb5d0921fea49fff362c1ba2b7e812520b58
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-03 16:30:16 +0530

    Update upload.py

--- AnkiVector/modules/upload.py
+++ AnkiVector/modules/upload.py
@@ -184,7 +184,7 @@ __help__ = """
  ⦁ /transfersh : reply to a telegram file to upload it on transfersh and get direct download link
  ⦁ /tmpninja : reply to a telegram file to upload it on tmpninja and get direct download link
 
-© @Damantha_Jasinghe 🇱🇰
+© @TheCreatorPavan
 """
 
-__mod_name__ = "File To Link"
+__mod_name__ = "ꜰɪʟᴇ ᴛᴏ ʟɪɴᴋ"
