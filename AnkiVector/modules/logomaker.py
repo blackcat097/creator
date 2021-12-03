05a4414e4e2a69924e1d4c0fd9e126d80d5acdd9
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-03 15:53:52 +0530

    Update logomaker.py

--- AnkiVector/modules/logomaker.py
+++ AnkiVector/modules/logomaker.py
@@ -252,7 +252,7 @@ async def logo_gen(event):
         await event.client.send_file(
             event.chat_id,
             file=flnme,
-            caption=f"Logo by AnkiVector",
+            caption=f"Logo by Creator Pavan",
             force_document=True,
         )
         os.remove(flnme)
@@ -371,11 +371,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "AnkiVectorLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheAnkiVectorBot ⚡️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By Creator Pavan Network")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Go to Help From  @AnkiSupport_Official, {e}')  
+   await event.reply(f'Error Go to Help From  @CreatorPavanSupport, {e}')  
   
   
   
@@ -429,5 +429,7 @@ __help__ = """
  ❍ /pandalogo text
  ❍ /spiderlogo text
  ❍ /spider_man_logo text
+
+© @TheCreatorPavan
  """
-__mod_name__ = "Logo Maker"
+__mod_name__ = "ʟᴏɢᴏ"
