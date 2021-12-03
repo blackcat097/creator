404ac978dfe3a65d1a275ba0e59787cd40b256ee
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-03 16:24:22 +0530

    Update yt.py

--- AnkiVector/modules/yt.py
+++ AnkiVector/modules/yt.py
@@ -1,5 +1,7 @@
 __help__ = """
  - /yt <text>: perform a youtube search
  - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+
+© @TheCreatorPavan
 """
-__mod_name__ = "Youtube"
+__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ"
