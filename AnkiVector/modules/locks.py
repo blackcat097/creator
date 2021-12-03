da08780f834734c21366a76a6851e9e626b996e5
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-12-03 15:50:33 +0530

    Update locks.py

--- AnkiVector/modules/locks.py
+++ AnkiVector/modules/locks.py
@@ -574,9 +574,11 @@ Locking bots will stop non-admins from adding bots to the chat.
 *Note:*
  • Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
  • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
+
+© @TheCreatorPavan
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "ʟᴏᴄᴋꜱ"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
